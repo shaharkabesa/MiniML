c5c255f1623eef102dd3af8d6933e7bf35edf741
shar <shaharkabasa@hotmail.com>
2025-11-27 22:33:24 +0200

    need to fix a bug within the saving the data into localstorage

--- static/js/main.js
+++ static/js/main.js
@@ -1,65 +1,76 @@
+    let color_name = document.getElementById("color_name");
+    let r = 0;
+    let g = 0;
+    let b = 0;
 
+    let color_pick = document.getElementById("color_pick");
 
-let r = 0;
-let g = 0;
-let b = 0;    
-
-let color_name = document.getElementById("color_name");
-
-let color_box = document.getElementById("color_box");
-
-
-let form = document.getElementById("color_pick");
-
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-})
-
-
-function grabGenColor() {
-    let green_input = document.getElementById("color_stats_g");
-
-    green_input.addEventListener("input", (e) => {
-        g = e.target.value;
-        console.log("red amount: " + g);
-        changeColorBox(r,g,b); 
+    color_pick.addEventListener("submit", (e) => {
+        e.preventDefault()
     })
-};
+    
+  function previewColors() {
+        
+        let green_input = document.getElementById("color_stats_g");
+        let red_input = document.getElementById("color_stats_r");
+        let blue_input = document.getElementById("color_stats_b");
+        
+        green_input.addEventListener("input", (e) => {
+            g = e.target.value;
+            console.log("red amount: " + g);
+            changeColorBox(r,g,b); 
+        })
+        
+        red_input.addEventListener("input", (e) => {
+            r = e.target.value;
+            console.log("red amount: " + g);
+            changeColorBox(r,g,b); 
+        })
+        
+        blue_input.addEventListener("input", (e) => {
+            b = e.target.value;
+            console.log("red amount: " + g);
+            changeColorBox(r,g,b); 
+        })
 
-
-function grabRedColor() {
-    let red_input = document.getElementById("color_stats_r");
-
-    red_input.addEventListener("input", (e) => {
-        r = e.target.value;
-        console.log("red amount: " + r);
-        changeColorBox(r,g,b); 
-    })
-};
-
-
-function grabBlueColor() {
-    let blue_input = document.getElementById("color_stats_b");
-
-    blue_input.addEventListener("input", (e) => {
-        b = e.target.value;
-        console.log("blue amount: " + b); 
-        changeColorBox(r,g,b);
-    })
-};
-
-function changeColorBox(r,g,b) {
-
-    if (color_box) {
-        color_box.style.backgroundColor = (`rgb(${r},${g},${b})`);
+        
     }
+
+    function createColorObject(r,g,b,color_name) {
+        let color_data = {
+            red_color: r,
+            green_color: g,
+            blue_color: b,
+            color_name: color_name
+        };
+
+        color_data = JSON.stringify(color_data); 
+        localStorage.setItem("color_data", color_data);
+        console.log("Color uploaded to localstorage");
+
+    }
+
+    function changeColorBox(r,g,b) {
+
+        if (color_box) {
+            color_box.style.backgroundColor = (`rgb(${r},${g},${b})`);
+        }
+    }
+
+
+function handleResearchButton() {
+
+    color_name = color_name.value;
+    if(color_name) {
+        let researchbtn = document.getElementById("startResearch");
+    researchbtn.addEventListener("click", (e) => {
+        
+        
+     
+        createColorObject(r,g,b, color_name);
+    });
+    }
+    
 }
-
-
-
-
-grabGreenColor();
-grabRedColor();
-grabBlueColor();
-
+handleResearchButton();
+previewColors();
